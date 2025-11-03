e528294a4ce2c5e26272c241e938c1a7699d3e3b
AaryanS21 <aaryan.sharma0411@gmail.com>
2025-11-03 19:25:41 +0000

    Add files via upload

--- GUIcal.py
+++ GUIcal.py
@@ -6,7 +6,7 @@ def showcalendar():
     win.geometry('550x600')
     fetch_year = int(year_field.get())
     Cal_content = calendar.calendar(fetch_year)
-    Cal_year = Label(win,text='Cal_content',font='Consolas 10 bold')
+    Cal_year = Label(win,text= Cal_content,font='Consolas 10 bold')
     Cal_year.grid(row=5,column=1,padx=20)
 
 
@@ -22,13 +22,17 @@ if __name__=='__main__':
     root.geometry('250x150')
     cal = Label(root,text='Calendar',bg='Red',font=('times',28,'bold'))
     year = Label(root,text='Enter year')
-    year_field = Entry(root)
+    
 
-    btn = Button(root,text='Show',bd=6,background='blue',
+    year_field = Entry(root)
+    year_field.grid(row=2,column=2)
+    year.grid(row=3,column=3)
+    
+    btn = Button(root,text='Show',bd=6,background='white',
                 activebackground='green', foreground='white', command=showcalendar)
     btn.grid(row=4,column=1)
 
-    btn2 = Button(root,text='Exit',bd=6,background='blue',
+    btn2 = Button(root,text='Exit',bd=6,background='white',
                 activebackground='green', foreground='white', command=root.destroy)
     btn2.grid(row=5,column=1)
 
@@ -38,5 +42,5 @@ if __name__=='__main__':
 
 
 
-    root.mainloop
+    root.mainloop()
     
